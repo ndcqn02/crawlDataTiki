39894532974e2f723aa95555aebf8e4194ef3042
Phan Kien <phankien110302@gmail.com>
2023-04-19 15:59:31 +0700

    modify project

--- main.py
+++ main.py
@@ -1,52 +1,51 @@
 
 import sys
 import pandas as pd
-sys.path.append('models')
-sys.path.append('database')
-sys.path.append('service')
-import mysql.connector
-from craw_data import craw_data
-from connect_database import add_data_to_database
+sys.path.append('Data_Crawling/models')
+sys.path.append('Data_Crawling/service')
+import craw_data 
 import until_data
-from fastapi import FastAPI
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="12345",
-    database="tiki"
-)
-
+from fastapi import FastAPI, HTTPException
 
 app_craw = FastAPI()
 
-
 @app_craw.get("/craw-product")
-def craw_product():
+async def craw_product():
     result = []
-    for i in range(1, 50):
-        result.append(craw_data(until_data.link_api, i,
-                      until_data.params, until_data.headers))
-    df_product = pd.DataFrame(result)
-    df_product.to_csv('crawled_data_ncds.csv', index=False)
-    add_data_to_database(df_product, until_data.user_name,
-                         until_data.password, until_data.database)
-    return result
-
+    if(HTTPException(200)):
+        for i in range(1, 50):
+            result.append(craw_data.craw_data(until_data.link_api, i,
+                    until_data.params, until_data.headers))
+        return {
+            "status": '200',
+            'message':'success',
+            'data' : result
+        }
+    else:
+        return {
+            "status": '400',
+            'message':HTTPException(400),
+            'data' : result
+        }
 
 @app_craw.get("/search")
 async def search_data(product_name: str):
-    cursor = mydb.cursor()
-    query = "SELECT * FROM product WHERE name LIKE %s"
-    value = (f"%{product_name}%",)
-    cursor.execute(query, value)
-#     print(type(cursor.fetchall()))
-    results = cursor.fetchall()
-    return {"data": results}
+    if(HTTPException(200)):
+        result = craw_data.search_data(product_name)
+        return {
+            'status' : '200',
+            'message' : 'success',
+            'data' : result
+        }
+    else:
+        return{
+            'status' : '400',
+            'message' : 'error'
+        }
 
 
 @app_craw.put("/update_data/{product_id}")
-def update_product(product_id: int, name: str):
+async def update_product(product_id: int, name: str):
     cursor = mydb.cursor()
     query = "UPDATE product SET name=%s WHERE id=%s"
     cursor.execute(query, (name, product_id))
@@ -55,14 +54,10 @@ def update_product(product_id: int, name: str):
     return {"message": "User updated"}
 
 
-@app_craw.delete("/delete_data/{product_id}")
-def delete_data(product_id: int):
+@app_craw.delete("/delete_data?product_id")
+async def delete_data(product_id):
     try:
-        cursor = mydb.cursor()
-        query = "DELETE FROM product WHERE id=%s"
-        cursor.execute(query, (product_id,))
-        mydb.commit()
-        cursor.close()
+        craw_data.delete_product(id)
         return {"message": "Data has been deleted successfully!"}
     except Exception as e:
         return {"error": str(e)}
